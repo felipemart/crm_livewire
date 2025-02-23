0cdcea0ec7f4cd3a0fbab59528e7dc6918416ded
lucyos <felipemartinsoliveira@gmail.com>
2025-02-23 11:27:03 -0300

    create logout test

--- tests/Feature/Livewire/Auth/LogoutTest.php
+++ tests/Feature/Livewire/Auth/LogoutTest.php
@@ -3,9 +3,16 @@
 declare(strict_types = 1);
 
 use App\Livewire\Auth\Logout;
+use App\Models\User;
 use Livewire\Livewire;
 
-it('renders successfully', function () {
+use function Pest\Laravel\actingAs;
+
+it('should be able to logout', function () {
+    $user = User::factory()->create();
+    actingAs($user);
     Livewire::test(Logout::class)
-        ->assertStatus(200);
-})->skip();
+        ->assertRedirect(route('login'));
+
+    expect(auth()->guest())->toBeTrue();
+});
